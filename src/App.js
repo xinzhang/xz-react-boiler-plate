93f23f58c4b1cbddc1dc9abe5a626e126dfa2351
Xin Zhang <xin.zhang@sca.com.au>
2018-12-13 15:04:01 +1100

    update folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Greeting from './Greeting';
-import Demo1 from './Demo1';
-import Demo2 from './Demo2';
-import Demo3 from './Demo3';
-import Demo4 from './Demo4';
-import UseEffectDemo from './UseEffectDemo';
+import Greeting from './hooks-quick-demos/Greeting';
+import Demo1 from './hooks-quick-demos/Demo1';
+import Demo2 from './hooks-quick-demos/Demo2';
+import Demo3 from './hooks-quick-demos/Demo3';
+import Demo4 from './hooks-quick-demos/Demo4';
+import UseEffectDemo from './hooks-quick-demos/UseEffectDemo';
 import ReduxHook from './redux-hooks/ReduxHook';
 import UseReducerDemo from './useReducerDemo';
 
